e944383b5fa6aa4888a3a629093f1cb0ec91886b
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-11-09 16:33:44 +0100

    Modified spec to included fizzbuzz check and coded fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,15 @@
 def fizzbuzz(number)
-    if number == 5
-      'buzz'
-    else
-      'fizz'
-    end
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"    
+  else
+    number
+  end
+end
+
+(1..20).each do |number|
+  fizzbuzz(number)
 end
