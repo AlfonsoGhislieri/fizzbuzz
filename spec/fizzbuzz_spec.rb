e8df6adb27f9df4b5f13167b17e92fbe055610dc
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-11-09 17:24:12 +0100

    Made hash test for rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,29 @@
 require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  test = {
+    1 => 1,
+    2 => 2,
+    3 => "fizz",
+    4 => 4,
+    5 => "buzz",
+    6 => "fizz",
+    7 => 7,
+    8 => 8,
+    9 => "fizz",
+    10 => "buzz",
+    11 => 11,
+    12 => "fizz",
+    13 => 13,
+    14 => 14,
+    15 => "fizzbuzz"
+  }
+
+  test.each do |key, value|
+    it "expects #{value} when the input is #{key}" do
+      result = fizzbuzz(key)
+      expect(result).to eq value
+    end
+  end
+
+end
